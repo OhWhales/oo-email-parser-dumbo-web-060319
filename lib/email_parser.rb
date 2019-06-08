683640426998282715b0a031855a84f68311c569
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-08 23:23:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailParser
     x = @emails.split(" ")
     x.each do |email|
       temp = email 
-      temp = temp.delete ","
+      temp = temp.delete(",")
       email = temp
     end
     puts x
